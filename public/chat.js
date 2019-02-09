5e7e35ed3fc9f0cf4ea62ca82a3f758b2becfe8d
Hrvoje Cavar <harwey.ch@gmail.com>
2019-02-09 16:17:31 +0100

    test

--- public/chat.js
+++ public/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('https://secret-cliffs-83069.herokuapp.com');
+var socket = io.connect('http://localhost:3000' || 'https://secret-cliffs-83069.herokuapp.com/');
 
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
